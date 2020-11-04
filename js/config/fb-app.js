19080b734157b54b69c0c314d6583d7032fe7534
Sean Narvasa <sean.narvasa@sprintshipworldwide.com>
2020-11-05 02:03:42 +0800

    Add firebase config

---
+++ js/config/fb-app.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDKszM64g46_MoNmqhMQT-hyH1n-yHdmyY",
+    authDomain: "erovoutika-internet-of-things.firebaseapp.com",
+    databaseURL: "https://erovoutika-internet-of-things.firebaseio.com",
+    projectId: "erovoutika-internet-of-things",
+    storageBucket: "erovoutika-internet-of-things.appspot.com",
+    messagingSenderId: "420446257049",
+    appId: "1:420446257049:web:54412d73fad71e04f45d08",
+    measurementId: "G-QBD41S27HZ"
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
